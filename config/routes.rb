a31940f8f9d0e786c638ce6e51c9c530f71f04ac
Piotr Król <piotr.krol@codesthq.com>
2018-08-19 13:48:35 +0200

    add .rubocop.yml to project and small changes after CR

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: 'new_article'
-  get 'articles/:id', to: 'articles#show', as: 'article'
-  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-  post 'articles', to: 'articles#create'
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
 end
