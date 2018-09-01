8b79d17c7bc4c49a9dc5606701dfe3355f5922c8
Piotr Król <piotr.krol@codesthq.com>
2018-09-01 10:22:40 +0200

    create destroy action for comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: %i[create destroy]
   end
 end
