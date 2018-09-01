1e98c81bb680c3b9fb952e66de1c58391105cb57
Piotr Król <piotr.krol@codesthq.com>
2018-09-01 14:49:17 +0200

    create email validator and use for commenter in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
-  validates :commenter, presence: true
-  validates :body, presence: true, length: { in: 6..500 }
+  validates :commenter, presence: true, email: true
+  validates :body, presence: true, length: { in: 6..500 }, bank_account: true
 end
