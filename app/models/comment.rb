2e04ea0b9e58dc827085dc35ce2409b2032c771b
Piotr Król <piotr.krol@codesthq.com>
2018-09-02 09:42:52 +0200

    add css styles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :article
 
   validates :commenter, presence: true, email: true
-  validates :body, presence: true, length: { in: 6..500 }, bank_account: true
+  validates :body, presence: true, length: { in: 6..500 }
 end
